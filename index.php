07b791ff3f947b653829f1699cebdbf736fed775
intern-tester <115193630+intern-tester@users.noreply.github.com>
2022-10-07 01:53:31 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,34 @@
 <?php 
 $namesite = getenv('API');
 
-print 'Test Cathedra VAR % ' . $namesite;
+function boostrap($content, $title = "Page"){
+return '
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>'.$title.'</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </head>
+  <body>
+    '.$content.'
+  </body>
+</html>
+';
+}
+
+//print 'Test Cathedra VAR % ' . $namesite;
+
+$content = '<h1>Cathedra</h1>
+<div class="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <p>© 2022 Company, Inc. All rights reserved.</p>
+      <ul class="list-unstyled d-flex"></ul>
+    </div>
+';
+
+print boostrap($content, 'Home page');
 
 
 ?>
