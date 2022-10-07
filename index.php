9a3bbebf114af71170f3df160da938960b54d47b
intern-tester <115193630+intern-tester@users.noreply.github.com>
2022-10-07 12:38:03 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if ($result['message']['text'] == '/start') {
 }elseif ($result['message']['text'] == '/doc') {
     sendDocument($result['message']['chat']['id'], 'https://www.nuozu.edu.ua/images/Onas/Pidrozdil/burlakova.jpg');
 }elseif ($result['message']['text'] == '/debug') {
-    file_put_contents('dump.txt', var_dump($result));
+    file_put_contents('dump.txt', var_dump($result['message']));
     sendDocument($result['message']['chat']['id'], 'dump.txt');
     unlink('dump.txt');
 }
